af06f7769a3f5e74e260e2c32aeee1acb006cd4c
Milan <igrok5554545@gmail.com>
2023-08-20 18:45:09 +0300

    first commit

---
+++ src/captcha/lib/HttpClient.cs
@@ -0,0 +1,21 @@
+﻿namespace DomainsParser;
+
+using System.Net.Http;
+
+public class Client
+{   
+    public string BasePath { get; private set; }
+
+    public string APIKey { get; private set; }
+
+    public HttpClient HttpClient {get; private set;}
+
+    public Client(string basePath, string APIKey)
+    {
+        this.BasePath = basePath;
+        this.APIKey = APIKey;
+        this.HttpClient = new HttpClient() {
+            BaseAddress = new Uri(this.BasePath)
+        };
+    }
+}
